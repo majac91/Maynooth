c349bf78dcba655d7d8c568bf277a12af336c955
Maya <majacvetkovic@outlook.com>
2020-07-21 22:25:46 +0200

    Header carousel

--- js/main.js
+++ js/main.js
@@ -9,19 +9,55 @@ menuToggler.addEventListener('click', function () {
     body.classList.toggle('open');
 });
 
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:5
-        }
-    }
-})
+
+$(".owl-carousel").on("initialized.owl.carousel", () => {
+    setTimeout(() => {
+      $(".owl-item.active .owl-slide-animated").addClass("is-transitioned");
+      $("section").show();
+    }, 200);
+  });
+  
+  const $owlCarousel = $(".owl-carousel").owlCarousel({
+    items: 1,
+    loop: true,
+    nav: true,
+  });
+  
+  $owlCarousel.on("changed.owl.carousel", e => {
+    $(".owl-slide-animated").removeClass("is-transitioned");
+  
+    const $currentOwlItem = $(".owl-item").eq(e.item.index);
+    $currentOwlItem.find(".owl-slide-animated").addClass("is-transitioned");
+  
+    const $target = $currentOwlItem.find(".owl-slide-text");
+    doDotsCalculations($target);
+  });
+  
+  $owlCarousel.on("resize.owl.carousel", () => {
+    setTimeout(() => {
+      setOwlDotsPosition();
+    }, 50);
+  });
+  
+  /*if there isn't content underneath the carousel*/
+  // $owlCarousel.trigger("refresh.owl.carousel");
+  
+  setOwlDotsPosition();
+  
+  function setOwlDotsPosition() {
+    const $target = $(".owl-item.active .owl-slide-text");
+    doDotsCalculations($target);
+  }
+  
+  function doDotsCalculations(el) {
+    const height = el.height();
+    const {top, left} = el.position();
+    const res = height + top + 100;
+  
+    $(".owl-carousel .owl-dots").css({
+      top: `${res}px`,
+      left: `${left}px`
+    });
+  }
+  
+

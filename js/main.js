1ef3679471ee7688cf92eadf247b7a2ada020c9f
Maya <majacvetkovic@outlook.com>
2020-07-21 17:51:49 +0200

    added Owl carousel

--- js/main.js
+++ js/main.js
@@ -8,3 +8,20 @@ let body = selectElement('body');
 menuToggler.addEventListener('click', function () {
     body.classList.toggle('open');
 });
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
